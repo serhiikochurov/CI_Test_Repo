721bbf3e038d777eb6f78a51478fa8c28cd40930
Sergei Kochurov <skochurov@4tifier.com>
2018-05-19 15:28:17 +0300

    Initial commit

---
+++ Assets/Editor/CustomBuild.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class CustomBuild
+{
+    public static void BuildAndroid()
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+
+        options.locationPathName = Application.dataPath;
+        options.options = BuildOptions.AcceptExternalModificationsToPlayer;
+        options.scenes = GetScenes();
+        options.target = BuildTarget.Android;
+        options.targetGroup = BuildTargetGroup.Android;
+        
+        BuildPipeline.BuildPlayer(options);
+    }
+    
+    private static string[] GetScenes()
+    {
+        var projectScenes = EditorBuildSettings.scenes;
+        List<string> scenesToBuild = new List<string>();
+        for (int i = 0; i < projectScenes.Length; i++)
+        {
+            if (projectScenes[i].enabled) {
+                scenesToBuild.Add(projectScenes[i].path);
+            }
+        }
+        return scenesToBuild.ToArray();
+    }
+}
